18a3f36d31de1cb6ad0640d67a04efbb60cd3a68
Rico4eT <shiwawa96@mail.ru>
2024-01-24 15:41:02 +0500

    My First While Loops

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
-
+        int power_degree = 1;
+        int counter = 0;
+        if (power < 0) {
+            System.out.println("too much power");
+        } else {
+            while (counter <= power) {
+                System.out.println(power_degree);
+                counter++;
+                power_degree *= 2;
+            }
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -1,6 +1,22 @@
 package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
-    public void printPrimeNumbers(int printToInclusive) {
+    public static void printPrimeNumbers(int printToInclusive) {
+        int divider = 2;
+        while (divider <= printToInclusive) {
+            boolean isPrime = true;
+            int dividend = 2;
+            while (dividend < divider){
+                if (divider % dividend == 0){
+                    isPrime = false;
+                    break;
+                }
+                dividend++;
+            }
+            if(isPrime) {
+                System.out.println(divider);
+            }
+            divider++;
+        }
     }
 }
